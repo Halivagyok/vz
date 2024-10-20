deebc58696d3852d5111718eb913b791ce1ebc47
Hali <nemesz06@gmail.com>
2024-10-20 21:40:25 +0200

    4

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const Wurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 
 //ido
-async function napKor(lat, lot){
-    
+async function nap(lat, lon){
+    const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&formatted=0`
+    const val = await fetch(url)
+    const adat = await val.json()
+
+    const napf = new Date(adat.results.sunrise)
+    const napl = new Date(adat.results.sunrset)
+
 }
 
 
